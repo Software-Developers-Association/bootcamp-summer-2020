75cb11b51f5a0cc77506ad74bc98a800b8d6f0b4
Kevin Pacheco <k.pacheco@me.com>
2020-08-10 12:51:22 -0700

    InputField now supports leading and trailing icons as per Material Design guidelines.

--- 2-2-react-tailwind/src/App.js
+++ 2-2-react-tailwind/src/App.js
@@ -1,10 +1,14 @@
 import React from 'react';
 import ResortsExample from './components/ResortsExample';
 import InputField from './components/InputField';
+import {ReactComponent as Heart} from './assets/icon-heart.svg';
+import {ReactComponent as Star} from './assets/icon-star.svg';
 
 function App() {
   return (
-    <InputField />
+    <div className="p-20">
+      <InputField iconStart={Heart} iconEnd={Star} />
+    </div>
   );
 }
 
--- 2-2-react-tailwind/src/components/InputField.jsx
+++ 2-2-react-tailwind/src/components/InputField.jsx
@@ -1,17 +1,33 @@
-import React, {useState} from 'react';
+import React from 'react';
+import PropTypes from 'prop-types';
+
+const propTypes = {
+    iconStart: PropTypes.object,
+    iconEnd: PropTypes.object
+};
 
 export default function InputField(props) {
+    const { iconStart:IconStart = undefined, iconEnd:IconEnd = undefined } = props;
+
+    const additionalInputContainerClasses = `${IconStart ? 'material-input-with-icon-start': ''} ${IconEnd ? 'material-input-with-icon-end' : ''}`;
+
     return (
-        <div className="relative m-10 inline-block h-12 border-b border-black border-opacity-25">
-            <input
-                className="material-input"
-                required
-                pattern=".*"
-                />
+        <div className={`material-input-container ${additionalInputContainerClasses}`}>
+            {
+                IconStart && <IconStart className="material-input-icon material-input-icon-start" />
+            }
+            {
+                IconEnd && <IconEnd className="material-input-icon material-input-icon-end" />
+            }
+            <input className="material-input" required pattern=".*" />
             <label className="material-input-label">
-                <span className="px-2">Label</span>
+                <span className="material-input-label-inner">
+                    Label
+                </span>
             </label>
-            <div className="material-input-underline"></div>
+            <div className="material-input-border"></div>
         </div>
     );
 }
+
+InputField.propTypes = propTypes;
